7ed95995c499cf1c72f4c4931fd7f99803087711
Ewelina Makowska <48392857+ewelinaMakowska@users.noreply.github.com>
2021-04-11 18:04:12 +0200

    temporary change

--- src/services/Api.js
+++ src/services/Api.js
@@ -20,10 +20,16 @@ let instance;
 if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'testing' || !process.env.NODE_ENV) {
   console.log('ello dev/testing')
 
-  instance =
+/*   instance =
   axios.create({
     baseURL: `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`
+  }) */
+
+  instance =
+  axios.create({
+    baseURL: `https://companies-app-backend.herokuapp.com:80`
   })
+  
 } else if (process.env.NODE_ENV === 'production') {
   console.log('ello')
 
