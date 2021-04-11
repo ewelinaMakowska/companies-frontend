eac6aa0e17babc304401017535bc7a5137c50ef4
Ewelina Makowska <48392857+ewelinaMakowska@users.noreply.github.com>
2021-04-11 18:49:47 +0200

    setup for heroku non-programmatic routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+//const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 //app.use(serveStatic(path.join(__dirname, 'dist')));
@@ -8,3 +8,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
 app.listen(port);
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
